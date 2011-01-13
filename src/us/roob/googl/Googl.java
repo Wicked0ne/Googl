731ec7ad8dcdcb2b7be8d5796fd91fd8a5c8d5c4
Mike <dev@roob.us>
2011-01-13 17:00:40 -0500

    First working bit of code...needs to be cleaned and organized.

--- src/us/roob/googl/Googl.java
+++ src/us/roob/googl/Googl.java
@@ -1,15 +1,64 @@
 package us.roob.googl;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Googl {
-	
-	public static String shortenURL(String url){
-		
-		return "";
+
+    private static final String apiKey = "12345";
+
+    public static String shortenURL(String urlToShorten) {
+
+	String urlParameters = "{\"longUrl\": \"" + urlToShorten + "\"}";
+	String apiURL = "https://www.googleapis.com/urlshortener/v1/url?key=" + apiKey;
+	BufferedReader rd = null;
+	StringBuilder sb = null;
+	String line = null;
+
+	try {
+	    URL url = new URL(apiURL);
+	    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+	    connection.setDoOutput(true);
+	    connection.setDoInput(true);
+	    connection.setInstanceFollowRedirects(false);
+	    connection.setRequestMethod("POST");
+	    connection.setRequestProperty("Content-Type", "application/json");
+	    // connection.setRequestProperty("charset", "utf-8");
+	    connection.setRequestProperty("Content-Length", "" + Integer.toString(urlParameters.getBytes().length));
+	    connection.setUseCaches(false);
+
+	    DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+	    wr.writeBytes(urlParameters);
+	    wr.flush();
+	    wr.close();
+	    connection.getInputStream();
+	    // read the result from the server
+	    rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	    sb = new StringBuilder();
+
+	    while ((line = rd.readLine()) != null) {
+		sb.append(line + '\n');
+	    }
+
+	    System.out.println(sb.toString());
+
+	    connection.disconnect();
+
+	} catch (Exception e) {
+
 	}
-	
-	public static String expandURL(String url){
-		
-		return "";
-	}
+	return "";
+    }
+
+    public static String expandURL(String url) {
+
+	return "";
+    }
 
 }
--- src/us/roob/googl/GooglTest.java
+++ src/us/roob/googl/GooglTest.java
@@ -1,5 +1,9 @@
 package us.roob.googl;
 
 public class GooglTest {
+    
+    public static void main(String[] args){
+	System.out.println(Googl.shortenURL("https://code.google.com/apis/console/"));
+    }
 
 }
