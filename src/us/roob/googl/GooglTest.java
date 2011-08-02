29fa77312e3d3eff9b134f162979b783eae89d56
Mike <dev@roob.us>
2011-08-01 20:06:40 -0400

    Changed default test url

--- src/us/roob/googl/GooglTest.java
+++ src/us/roob/googl/GooglTest.java
@@ -3,7 +3,7 @@ package us.roob.googl;
 public class GooglTest {
     
     public static void main(String[] args){
-	String url = "gallery.roob.us:88";
+	String url = "http://www.google.com";
 	System.out.println("Original url: " + url);
 	System.out.println("Shortened url: " + Googl.shortenURL(url));
 	System.out.println("Expanded url: " + Googl.expandURL(Googl.shortenURL(url)));
